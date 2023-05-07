81bff8d5ba7d9e5a2162672cd34eca486c25a806
Niels Schultz <niels_schultz@web.de>
2023-05-07 16:52:14 +0200

    added services

--- SimplePasswordManager/App.xaml.cs
+++ SimplePasswordManager/App.xaml.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using SimplePasswordManager.Services;
+using Microsoft.Extensions.DependencyInjection;
+using SimplePasswordManager.Core;
+using SimplePasswordManager.MVVM.ViewModel;
 
 namespace SimplePasswordManager
 {
@@ -13,5 +17,38 @@ namespace SimplePasswordManager
     /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddSingleton<MainWindow>(provider => new MainWindow()
+            {
+                DataContext = provider.GetRequiredService<MainViewModel>()
+            });
+            
+            // Add all ViewModels
+            services.AddSingleton<MainViewModel>();
+            services.AddSingleton<HomeViewModel>();
+            services.AddSingleton<GernatePasswordViewModel>();
+            services.AddSingleton<ManagePasswordsViewModel>();
+
+            services.AddSingleton<INavigationService, NavigationService>();
+
+            services.AddSingleton<Func<Type, ViewModel>>(serviceProvider =>
+                viewModelType => (ViewModel)serviceProvider.GetRequiredService(viewModelType));
+            
+            // build provider
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
