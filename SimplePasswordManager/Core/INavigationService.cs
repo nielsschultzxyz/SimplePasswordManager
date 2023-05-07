3f3484396029f66a89fe81ace4a967c01029d904
Niels Schultz <niels_schultz@web.de>
2023-05-07 16:28:10 +0200

    Added NavigationService.cs

---
+++ SimplePasswordManager/Core/INavigationService.cs
@@ -0,0 +1,7 @@
+﻿namespace SimplePasswordManager.Core;
+
+public interface INavigationService
+{
+    ViewModel CurrentView { get; }
+    void NavigateTo<T>() where T : ViewModel;
+}
---
+++ SimplePasswordManager/Services/NavigationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using SimplePasswordManager.Core;
+
+namespace SimplePasswordManager.Services;
+
+public class NavigationService : ObservableObject, INavigationService
+{
+    private Func<Type, ViewModel> _viewModelFactory;
+    
+    private ViewModel _currentView;
+    public ViewModel CurrentView
+    {
+        get => _currentView;
+        set
+        {
+            _currentView = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public NavigationService(Func<Type, ViewModel> viewModelFactory)
+    {
+        _viewModelFactory = viewModelFactory;
+    }
+    
+    public void NavigateTo<TViewModel>() where TViewModel : ViewModel
+    {
+        ViewModel viewModel = _viewModelFactory.Invoke(typeof(TViewModel));
+        CurrentView = viewModel;
+    }
+}
