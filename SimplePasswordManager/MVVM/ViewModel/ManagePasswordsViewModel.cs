dd35fbd2f4fa997945a5d6b9574d982de8ff3dd8
Niels Schultz <niels_schultz@web.de>
2023-05-10 21:12:35 +0200

    fields + binding values

--- SimplePasswordManager/MVVM/ViewModel/ManagePasswordsViewModel.cs
+++ SimplePasswordManager/MVVM/ViewModel/ManagePasswordsViewModel.cs
@@ -1,6 +1,46 @@
-﻿namespace SimplePasswordManager.MVVM.ViewModel;
+﻿using System.Windows.Controls;
+
+namespace SimplePasswordManager.MVVM.ViewModel;
 
 public class ManagePasswordsViewModel : Core.ViewModel
 {
+    private string _appPassword = "AppPassword";
+
+    public string AppPassword
+    {
+        get => _appPassword;
+        set
+        {
+            _appPassword = value;
+            OnPropertyChanged();
+        }
+    }
     
+    private string _appName = "AppName";
+
+    public string AppName
+    {
+        get => _appName;
+        set
+        {
+            _appName = value;
+            OnPropertyChanged();
+        }
+    }
+    private string _appUsername = "Username";
+
+    public string AppUsername
+    {
+        get => _appUsername;
+        set
+        {
+            _appUsername = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ManagePasswordsViewModel()
+    {
+        
+    }
 }
